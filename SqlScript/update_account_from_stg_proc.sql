d318b57f2bf955f8b42016bac31231fc8906da1a
Ian <ilt.mec@gmail.com>
2019-12-16 12:14:41 -0800

    added stg.proc_account_update_from_stg()

---
+++ SqlScript/update_account_from_stg_proc.sql
@@ -0,0 +1,13 @@
+
+Create Function stg.proc_account_update_from_stg() RETURNS void
+AS $$
+
+    INSERT into public.account
+    select s.*
+    from stg.account s
+        LEFT join public.account p on s.id = p.id
+    where  s.systemmodstamp > p.systemmodstamp;
+
+
+$$
+LANGUAGE SQL
